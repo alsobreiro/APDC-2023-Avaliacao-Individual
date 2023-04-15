c706f416ee95b535e995b8d1a13c4881f1922e56
alsobreiro <50711544+alsobreiro@users.noreply.github.com>
2023-04-15 22:40:28 +0100

    Add files via upload

---
+++ src/main/java/pt/unl/fct/di/apdc/firstwebapp/util/DeleteOthers.java
@@ -0,0 +1,12 @@
+package pt.unl.fct.di.apdc.firstwebapp.util;
+
+public class DeleteOthers {
+    public String username;
+    public String userToDelete;
+    public DeleteOthers(){}
+    public DeleteOthers(String username, String userToDelete){
+        this.username = username;
+        this.userToDelete = userToDelete;
+    }
+
+}
